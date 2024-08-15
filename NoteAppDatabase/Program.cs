0d1092b5cb28ba750d5c71d3442cb742d19fdf56
Hilmi Özbay <ozbayyhilmi@gmail.com>
2024-08-15 15:50:13 +0300

    NoteApp

--- NoteAppDatabase/Program.cs
+++ NoteAppDatabase/Program.cs
@@ -1,33 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Proje5.Models;
-
 using Proje5.Utility;
 
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<UygulamaDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-//builder.Services.AddScoped<IRepository<Account>, Repository<Account>>();
-//builder.Services.AddScoped<IRepository<Notes>, Repository<Notes>>();
-//builder.Services.AddScoped<IRepository<Users>, Repository<Users>>();
-
-// Add session services
-builder.Services.AddDistributedMemoryCache(); // Adds a default in-memory implementation of IDistributedCache
+builder.Services.AddDistributedMemoryCache(); 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout (adjust as needed)
-    options.Cookie.HttpOnly = true; // Set the HttpOnly property of the session cookie
-    options.Cookie.IsEssential = true; // Make the session cookie essential for the application
+    options.IdleTimeout = TimeSpan.FromMinutes(30); 
+    options.Cookie.HttpOnly = true; 
+    options.Cookie.IsEssential = true; 
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -39,7 +31,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-// Use session middleware
+
 app.UseSession();
 
 app.UseAuthorization();
